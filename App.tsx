9bc2f60c15ed60178f001599d772040015bc09dc
Amer Hussain <amer.hussain2434@gmail.com>
2024-10-03 11:20:27 +0500

    Your commit message here

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { SafeAreaView, ScrollView, } from 'react-native';
+import { SafeAreaView, ScrollView, View, Text } from 'react-native';
 import React from 'react';
 import { useState, useEffect } from 'react';
 import FlatCards from './components/FlatCard';
 import Popular_Restaurant from './components/Popular_Restaurant';
 import SpalshScreen from './SplashScreen';
+import firestore from '@react-native-firebase/firestore';
+
 export default function App() {
   const [isShowSplash, setSplash] = useState(true);
   useEffect(() => {
@@ -24,6 +26,28 @@ export default function App() {
       </SafeAreaView>
     );
   }
+  // This is not a code only for testing purpose of firebase 
+  // const [myData, setmydata] = useState(null);
+  // useEffect(() => {
+  //   getDatabase();
+
+  // }, []);
+  // const getDatabase = async () => {
+  //   const docRef = firestore().collection('121').doc('Zi1pX1pVs6bESvebAo9h');
+  //   const doc = await docRef.get();
+
+  //   if (doc.exists) {
+  //     console.log(doc.data());  // This will log the actual document data (fields)
+  //     setmydata(doc.data());
+  //   } else {
+  //     console.log('No such document!');
+  //   }
+  // };
+  // return (
+  //   <View>
+  //     <Text> Say :-{myData ? myData.name : 'loading'}</Text>
+  //   </View>
+  // );
 }
 
 
